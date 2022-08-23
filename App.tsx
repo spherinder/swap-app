76b9b7a56e91bce8fedf86e17024e2e0e3a03f2d
obergqvist <obergqvist@student.eth.ch>
2022-08-24 01:37:38 +0200

    Install ApolloClient for graphql requests
    To install I run `npm install @apollo/client graphql`. After a `git
    pull` and `npm install`, the packages will be installed in your
    environment as well.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, ApolloError, DocumentNode } from '@apollo/client';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
