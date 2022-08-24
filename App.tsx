c336074230c32900ce12ee2fdf4a0648998572d1
obergqvist <obergqvist@student.eth.ch>
2022-08-24 02:25:08 +0200

    Configure Apollo client; hook up Hasura database
    I add a dummy component `<DisplayUsers/>` that fetches user data from the
    hasura server and puts them in a `ScrollView`. Currently the the DB just
    contains 3 dummy users.
    
    The database address is hardcoded to the *local IP-address
    of my laptop*. In the future, contributors ought to put their own
    database's IP-address in an environment variable, which git should
    ignore, and Apollo Client should read that address.

--- App.tsx
+++ App.tsx
@@ -2,15 +2,55 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, ApolloError
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
+type User = {
+  id: number;
+  first_name: string;
+  email_address: string;
+  bio: string;
+}
+
+type Users = {
+  user_profiles: Array<User>
+}
+
 const client = new ApolloClient({
-  uri: 'https://flyby-gateway.herokuapp.com/',
+  uri: 'http://192.168.43.50:8080/v1/graphql',
   cache: new InMemoryCache(),
 });
+
+const GET_USERS = gql`
+  query GetUsers {
+    user_profiles {
+      id
+      email_address
+      first_name
+      bio
+    }
+  }
+`;
+
+const DisplayUsers = () => {
+  const { loading, error, data } = useQuery<Users>(GET_USERS);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error || !data) return <Text>Error :</Text>;
+
+  const compArr = data.user_profiles.map(({id, first_name, email_address, bio}: User) => (
+    <View key={id}>
+      <Text style={{fontSize: 20}}>{'\n' + first_name + '\n' + email_address}</Text>
+      <Text style={{fontStyle: 'italic'}}>About this user:</Text>
+      <Text>{bio + '\n'}</Text>
+    </View>
+  ));
+  return (<ScrollView>{compArr}</ScrollView>);
+
+}
 
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
+      <DisplayUsers/>
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style="auto" />
